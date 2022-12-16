27ca8e14f6d64cb570eade7efdc2f4946d2aa4f2
Your Name <you@example.com>
2022-12-16 01:15:59 +0100

    static web server

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,15 @@
-const message: string = "Typescript works";
-console.log(message);
+import express from "express";
+import path from "path";
+
+const port = 3112;
+const app = express();
+const __dirname = path.resolve(path.dirname(""));
+
+// html, css und javascript (abo app not dynamic)
+app.use(express.static(__dirname + "/public"));
+// app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "./public/views"));
+
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
+});
