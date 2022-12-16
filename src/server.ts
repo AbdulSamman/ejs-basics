68cdca3f23bfa39b1929f9c26b83083eafc6b461
Your Name <you@example.com>
2022-12-16 01:51:54 +0100

    dummy navbar

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,18 @@ const __dirname = path.resolve(path.dirname(""));
 
 // html, css und javascript (abo app not dynamic)
 app.use(express.static(__dirname + "/public"));
-// app.set("view engine", "ejs");
-// app.set("views", path.join(__dirname, "./public/views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./public/views"));
+
+const appTitle = "The tech Book Site";
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.render("index", { appTitle });
+});
+
+app.get("/info", (req: express.Request, res: express.Response) => {
+  res.render("info", { appTitle });
+});
 
 app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`);
